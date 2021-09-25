a210251c52080a77b3b2c9da8fdf01cad5a20f23
Gersey Stelmach <gerseystelmach@gmail.com>
2021-09-25 18:43:53 +0200

    "Solution for quete 8"

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly{
 
     private boolean flying;
     private int altitude;
@@ -21,4 +21,45 @@ public class Eagle extends Bird {
     public String sing() {
         return "Screech!";
     }
+
+    public void takeOff() {
+        if (!this.flying && this.altitude == 0) {
+            this.flying = true;
+            System.out.println(this.getName() + "takes off in the sky");
+        }
+    }
+
+    public void flyUp(int meters) {
+        if (this.flying) {
+            this.altitude = Math.max(this.altitude + meters, 325);
+            System.out.println(this.getName() + "flies upward, altitude : " + this.altitude);
+        }
+
+    }
+
+    public void flyDown(int meters) {
+        if (this.flying) {
+            this.altitude = Math.min(this.altitude - meters, 0);
+            if (this.altitude < 0) {
+                this.altitude = 0;
+            }
+            System.out.println(this.getName() + "flies downward, altitude : " + this.altitude);
+        }
+    }
+
+
+    public void glide() {
+        if (this.flying) {
+            System.out.println("It glides into the air");
+        }
+    }
+
+    public void land() {
+        if (this.flying && this.altitude == 0) {
+            System.out.println(this.getName() + " lands on the ground.");
+        } else {
+            System.out.println(this.getName() + " is too high, it can't lands.");
+        }
+    }
 }
+
---
+++ Fly.java
@@ -0,0 +1,12 @@
+public interface Fly {
+
+    void takeOff();
+
+    void flyDown(int meters);
+
+    void flyUp(int meters);
+
+    void glide();
+
+    void land();
+}
